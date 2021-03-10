1006dc5584e75bdd150bf88ec6c8b2b817010f08
haze-yuuji <hazehaze1982@yahoo.co.jp>
2021-03-10 14:46:30 +0900

    Revert "Merge pull request #8 from haze-yuuji/second_training"
    This reverts commit 5f2de06b0373e057883a95efae0f7665d17c1db6, reversing
    changes made to e8d5f8b69db090e1dfb90a64cb682c4f52739d9d.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -7,7 +7,6 @@ class CalendarsController < ApplicationController
   end
 
   # 予定の保存
-  
   def create
     Plan.create(plan_params)
     redirect_to action: :index
@@ -16,9 +15,8 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
-
 
   def get_week
     w_days = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
@@ -36,14 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = Date.today.wday + x
-      # xはtimesメソッドと連携、その日以降の曜日を取得する
-      if wday_num >= 7
-        wday_num = wday_num -7
-      end
-
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: w_days[wday_num] }
+      days = { month:  (@todays_date + x).month, date:  (@todays_date+x).day, plans:  today_plans}
       @week_days.push(days)
     end
 
